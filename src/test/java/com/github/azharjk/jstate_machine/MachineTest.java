140d1fd9f2c3bb2718006fcbbc5e7bcace8d770e
azharjk <azhr.project@gmail.com>
2021-08-30 20:14:26 +0700

    refactor test code for more reusable code

--- src/test/java/com/github/azharjk/jstate_machine/MachineTest.java
+++ src/test/java/com/github/azharjk/jstate_machine/MachineTest.java
@@ -6,109 +6,95 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class MachineTest {
+  // helper method
+  private ArrayList<String> createMachineAndGetSignature(String param) {
+    Machine ret = new Machine(param);
+    return ret.getSignature();
+  }
+
+  private ArrayList<String> createExpectedResultSignature(String... params) {
+    ArrayList<String> ret = new ArrayList<String>();
+    for (String param : params) {
+      ret.add(param);
+    }
+    return ret;
+  }
+
+  private void assertMultipleSignature(int size, ArrayList<String> expected, ArrayList<String> actual) {
+    for (int i = 0; i < size; i++) {
+      Assert.assertEquals(expected.get(i), actual.get(i));
+    }
+  }
+
   @Test
   public void shouldHandleEndOfString() {
-    Machine m1 = new Machine("hello");
-    ArrayList<String> resultToTest_1 = m1.getSignature();
-
+    // test 1
+    ArrayList<String> resultToTest_1 = this.createMachineAndGetSignature("hello");
     Assert.assertEquals(1, resultToTest_1.size());
     Assert.assertEquals("hello", resultToTest_1.get(0));
 
-    Machine m2 = new Machine("covid-");
-    ArrayList<String> resultToTest_2 = m2.getSignature();
-
+    // test 2
+    ArrayList<String> resultToTest_2 = this.createMachineAndGetSignature("covid-");
     Assert.assertEquals(1, resultToTest_2.size());
     Assert.assertEquals("covid-", resultToTest_2.get(0));
  }
 
   @Test
   public void shouldHandleArrowSymbol() {
-    Machine m1 = new Machine("covid->19");
-    ArrayList<String> resultToTest_1 = m1.getSignature();
+    // test 1
+    ArrayList<String> resultToTest_1 = this.createMachineAndGetSignature("covid->19");
+    ArrayList<String> expectedResultTest_1 = this.createExpectedResultSignature("covid", "19");
     int size_1 = resultToTest_1.size();
 
-    ArrayList<String> parsedResultTest_1 = new ArrayList<String>();
-    parsedResultTest_1.add("covid");
-    parsedResultTest_1.add("19");
-
     Assert.assertEquals(2, size_1);
-    for (int i = 0; i < size_1; i++) {
-      Assert.assertEquals(parsedResultTest_1.get(i), resultToTest_1.get(i));
-    }
+    this.assertMultipleSignature(size_1, expectedResultTest_1, resultToTest_1);
 
-    Machine m2 = new Machine("covid->19->bad");
-    ArrayList<String> resultToTest_2 = m2.getSignature();
+    // test 2
+    ArrayList<String> resultToTest_2 = this.createMachineAndGetSignature("covid->19->bad");
+    ArrayList<String> expectedResultTest_2 = this.createExpectedResultSignature("covid", "19", "bad");
     int size_2 = resultToTest_2.size();
 
-    ArrayList<String> parsedResultTest_2 = new ArrayList<String>();
-    parsedResultTest_2.add("covid");
-    parsedResultTest_2.add("19");
-    parsedResultTest_2.add("bad");
-
     Assert.assertEquals(3, size_2);
-    for (int i = 0; i < size_2; i++) {
-      Assert.assertEquals(parsedResultTest_2.get(i), resultToTest_2.get(i));
-    }
+    this.assertMultipleSignature(size_2, expectedResultTest_2, resultToTest_2);
 
-    Machine m3 = new Machine("covid-19->bad");
-    ArrayList<String> resultToTest_3 = m3.getSignature();
+    // test 3
+    ArrayList<String> resultToTest_3 = this.createMachineAndGetSignature("covid-19->bad");
+    ArrayList<String> expectedResultTest_3 = this.createExpectedResultSignature("covid-19", "bad");
     int size_3 = resultToTest_3.size();
 
-    ArrayList<String> parsedResultTest_3 = new ArrayList<String>();
-    parsedResultTest_3.add("covid-19");
-    parsedResultTest_3.add("bad");
-
     Assert.assertEquals(2, size_3);
-    for (int i = 0; i < size_3; i++) {
-      Assert.assertEquals(parsedResultTest_3.get(i), resultToTest_3.get(i));
-    }
+    this.assertMultipleSignature(size_3, expectedResultTest_3, resultToTest_3);
   }
 
   @Test
   public void shouldHandleSpecialCase() {
-    Machine m1 = new Machine("");
-    ArrayList<String> resultToTest_1 = m1.getSignature();
-
+    // test 1
+    ArrayList<String> resultToTest_1 = this.createMachineAndGetSignature("");
     Assert.assertEquals(1, resultToTest_1.size());
     Assert.assertEquals("", resultToTest_1.get(0));
 
-    Machine m2 = new Machine("covid->");
-    ArrayList<String> resultToTest_2 = m2.getSignature();
+    // test 2
+    ArrayList<String> resultToTest_2 = this.createMachineAndGetSignature("covid->");
+    ArrayList<String> expectedResultTest_2 = this.createExpectedResultSignature("covid", "");
     int size_2 = resultToTest_2.size();
 
-    ArrayList<String> parsedResultTest_2 = new ArrayList<String>();
-    parsedResultTest_2.add("covid");
-    parsedResultTest_2.add("");
-
     Assert.assertEquals(2, size_2);
-    for (int i = 0; i < size_2; i++) {
-      Assert.assertEquals(parsedResultTest_2.get(i), resultToTest_2.get(i));
-    }
+    this.assertMultipleSignature(size_2, expectedResultTest_2, resultToTest_2);
 
-    Machine m3 = new Machine("->");
-    ArrayList<String> resultToTest_3 = m3.getSignature();
+    // test 3
+    ArrayList<String> resultToTest_3 = this.createMachineAndGetSignature("->");
+    ArrayList<String> expectedResultTest_3 = this.createExpectedResultSignature("", "");
     int size_3 = resultToTest_3.size();
 
-    ArrayList<String> parsedResultTest_3 = new ArrayList<String>();
-    parsedResultTest_3.add("");
-    parsedResultTest_3.add("");
-
     Assert.assertEquals(2, size_3);
-    for (int i = 0; i < size_3; i++) {
-      Assert.assertEquals(parsedResultTest_3.get(i), resultToTest_3.get(i));
-    }
+    this.assertMultipleSignature(size_3, expectedResultTest_3, resultToTest_3);
 
-    Machine m4 = new Machine("->covid");
-    ArrayList<String> resultToTest_4 = m4.getSignature();
+    // test 4
+    ArrayList<String> resultToTest_4 = this.createMachineAndGetSignature("->covid");
+    ArrayList<String> expectedResultTest_4 = this.createExpectedResultSignature("", "covid");
     int size_4 = resultToTest_4.size();
 
-    ArrayList<String> parsedResultTest_4 = new ArrayList<String>();
-    parsedResultTest_4.add("");
-    parsedResultTest_4.add("covid");
-
     Assert.assertEquals(2, size_4);
-    for (int i = 0; i < size_4; i++) {
-      Assert.assertEquals(parsedResultTest_4.get(i), resultToTest_4.get(i));
-    }
+    this.assertMultipleSignature(size_4, expectedResultTest_4, resultToTest_4);
   }
 }
